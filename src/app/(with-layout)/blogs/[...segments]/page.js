b59d1fa000de80fa39d7b0d29d93bb2a59d9e9d2
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-05-09 21:30:15 +0400

    83.10 useRouter, useParams, useSearchParams hooks.

--- src/app/(with-layout)/blogs/[...segments]/page.js
+++ src/app/(with-layout)/blogs/[...segments]/page.js
@@ -1,7 +1,17 @@
-import React from 'react';
+"use client"
 
-const SingleBlogPage = ({params}) => {
+import { useParams, useSearchParams } from "next/navigation";
+
+const SingleBlogPage = ({params, searchParams}) => {
   const [year, month, date, id] = params.segments || [];
+
+  // get params and query from client components.
+  const params2 = useParams();
+  const searchParams2 = useSearchParams();
+
+  console.log(params2.segments);
+  console.log(searchParams2.get("title"));
+
 
   return (
     <div>
@@ -10,6 +20,9 @@ const SingleBlogPage = ({params}) => {
         <li>{month}</li>
         <li>{date}</li>
         <li>{id}</li>
+
+        {/* receive the query title from single blog page */}
+        <li>{searchParams.title}</li>
       </ul>
     </div>
   );
