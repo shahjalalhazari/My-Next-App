f4713624d9cf24df5d1c1f4d36a908017de5255e
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2024-06-07 19:39:28 +0400

    84.5 Revalidating Data (ISR)

--- src/utils/getAllCategories.js
+++ src/utils/getAllCategories.js
@@ -1,7 +1,9 @@
 const getAllCategories = async() => {
-	const res = await fetch("http://localhost:5000/categories", {
-		cache:"force-cache"
-	});
+	const res = await fetch("http://localhost:5000/categories",
+		// { cache: "force-cache" }
+		{ next: { revalidate: 5, } } // background revalidation
+		// rebuild after each 5sec on background.
+	);
 	return res.json();
 };
 
